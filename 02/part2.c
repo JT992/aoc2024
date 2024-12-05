49bb7721f96dc4fca55039a68c566ce54d51528e
JT992 <jake@artran.co.uk>
2024-12-05 15:41:42 +0000

    02 still WIP

--- 02/part2.c
+++ 02/part2.c
@@ -6,15 +6,16 @@
 #define MAX_LEVELS 8
 
 /* test */
-/* #define FILE_NAME "test.txt" */
+#define FILE_NAME "test.txt"
 
 /* actual */
-#define FILE_NAME "input.txt"
+/* #define FILE_NAME "input.txt" */
 
 #define NEXT_INT_ON_LINE() strtol(head, &head, 10)
 
 typedef struct {
-  int a, b, c, z;
+  int prev;
+  int pane[4];
 } Window;
 
 typedef enum {
@@ -26,21 +27,23 @@ typedef enum {
 int slide_window(Window *window, int next) {
   if (!next)
     return 0;
-  window->z = window->a;
-  window->a = window->b;
-  window->b = window->c;
-  window->c = next;
+  int *pane = window->pane;
+  pane[0] = pane[1];
+  pane[1] = pane[2];
+  pane[2] = pane[3];
+  pane[3] = next;
   return 1;
 }
 
 int intcmp(int a, int b) { return (a > b) - (a < b); }
 
-int init_direction(Window window, int d) {
-  int ab = intcmp(window.a, window.b), bc = intcmp(window.b, window.c),
-      ac = intcmp(window.a, window.c), bd = intcmp(window.b, d),
-      cd = intcmp(window.c, d);
+int init_direction(Window window) {
+  int ab = intcmp(window.pane[0], window.pane[1]),
+      bc = intcmp(window.pane[1], window.pane[2]),
+      ac = intcmp(window.pane[0], window.pane[2]),
+      bd = intcmp(window.pane[1], window.pane[3]),
+      cd = intcmp(window.pane[2], window.pane[3]);
   int sum = ab + bc + ac + bd + cd;
-  /* printf("%d\n", sum); */
   if (sum > 0) {
     return 1;
   } else if (sum < 0) {
@@ -55,12 +58,19 @@ int is_pair_safe(int prev, int cur, int direction) {
   return (intcmp(prev, cur) == direction) && (diff >= 1 && diff <= 3);
 }
 
+Safeness init_window_safeness(Window window, int direction) {
+  return !is_pair_safe(window.pane[0], window.pane[1], direction);
+}
+
+Safeness final_window_safeness(Window window, int direction) {
+  return !is_pair_safe(window.pane[2], window.pane[3], direction);
+}
+
 Safeness window_safeness(Window window, int direction) {
-  if (is_pair_safe(window.a, window.b, direction)) {
+  if (is_pair_safe(window.pane[1], window.pane[2], direction)) {
     return VerySafe;
-  } else if (is_pair_safe(window.a, window.c, direction) ||
-             (is_pair_safe(window.b, window.c, direction) &&
-              is_pair_safe(window.z, window.b, direction))) {
+  } else if (is_pair_safe(window.pane[0], window.pane[2], direction) ||
+             is_pair_safe(window.pane[1], window.pane[3], direction)) {
     return KindaSafe;
   } else {
     return NotSafe;
@@ -73,23 +83,22 @@ int main() {
     return 0;
   char line[MAX_STRING_LEN];
   char *head;
-  int direction, safeness, first_d, number_safe = 0;
+  int i, direction, safeness, number_safe = 0;
   Window window;
   while (fgets(line, MAX_STRING_LEN, file)) {
     head = line;
     safeness = 2;
-    window = (Window){NEXT_INT_ON_LINE(), NEXT_INT_ON_LINE(),
-                      NEXT_INT_ON_LINE(), -10 /* just a sentinel */};
-    first_d = NEXT_INT_ON_LINE();
-    /* printf("%d %d %d %d: ", window.a, window.b, window.c, first_d); */
-    direction = init_direction(window, first_d);
-    safeness -= window_safeness(window, direction);
-    slide_window(&window, first_d);
+    for (i = 0; i < 4; i++) {
+      window.pane[i] = NEXT_INT_ON_LINE();
+    }
+    direction = init_direction(window);
+    safeness -= init_window_safeness(window, direction);
     while ((safeness -= window_safeness(window, direction)) > 0 &&
            slide_window(&window, NEXT_INT_ON_LINE()))
       ;
+    safeness -= final_window_safeness(window, direction);
+    printf("%d\n", safeness);
     number_safe += (safeness > 0);
-    /* printf("and its safeness is %d\n", safeness); */
   }
   printf("%d\n", number_safe);
   return 0;
