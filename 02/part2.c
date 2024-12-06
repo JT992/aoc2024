d30b063056a21d00a3634252644d8b56970c822a
JT992 <jake@artran.co.uk>
2024-12-06 16:01:42 +0000

    02 still still WIP

--- 02/part2.c
+++ 02/part2.c
@@ -6,17 +6,10 @@
 #define MAX_LEVELS 8
 
 /* test */
-#define FILE_NAME "test.txt"
+/* #define FILE_NAME "test.txt" */
 
 /* actual */
-/* #define FILE_NAME "input.txt" */
-
-#define NEXT_INT_ON_LINE() strtol(head, &head, 10)
-
-typedef struct {
-  int prev;
-  int pane[4];
-} Window;
+#define FILE_NAME "input.txt"
 
 typedef enum {
   VerySafe = 0,
@@ -24,25 +17,12 @@ typedef enum {
   NotSafe = 2,
 } Safeness;
 
-int slide_window(Window *window, int next) {
-  if (!next)
-    return 0;
-  int *pane = window->pane;
-  pane[0] = pane[1];
-  pane[1] = pane[2];
-  pane[2] = pane[3];
-  pane[3] = next;
-  return 1;
-}
-
 int intcmp(int a, int b) { return (a > b) - (a < b); }
 
-int init_direction(Window window) {
-  int ab = intcmp(window.pane[0], window.pane[1]),
-      bc = intcmp(window.pane[1], window.pane[2]),
-      ac = intcmp(window.pane[0], window.pane[2]),
-      bd = intcmp(window.pane[1], window.pane[3]),
-      cd = intcmp(window.pane[2], window.pane[3]);
+int init_direction(int *head) {
+  int ab = intcmp(head[0], head[1]), bc = intcmp(head[1], head[2]),
+      ac = intcmp(head[0], head[2]), bd = intcmp(head[1], head[3]),
+      cd = intcmp(head[2], head[3]);
   int sum = ab + bc + ac + bd + cd;
   if (sum > 0) {
     return 1;
@@ -58,19 +38,15 @@ int is_pair_safe(int prev, int cur, int direction) {
   return (intcmp(prev, cur) == direction) && (diff >= 1 && diff <= 3);
 }
 
-Safeness init_window_safeness(Window window, int direction) {
-  return !is_pair_safe(window.pane[0], window.pane[1], direction);
+Safeness window_edge_safeness(int *head, int direction) {
+  printf("%d %d ", head[0], head[1]);
+  return !is_pair_safe(head[0], head[1], direction);
 }
 
-Safeness final_window_safeness(Window window, int direction) {
-  return !is_pair_safe(window.pane[2], window.pane[3], direction);
-}
-
-Safeness window_safeness(Window window, int direction) {
-  if (is_pair_safe(window.pane[1], window.pane[2], direction)) {
+Safeness window_safeness(int *head, int direction) {
+  if (is_pair_safe(head[0], head[1], direction)) {
     return VerySafe;
-  } else if (is_pair_safe(window.pane[0], window.pane[2], direction) ||
-             is_pair_safe(window.pane[1], window.pane[3], direction)) {
+  } else if (is_pair_safe(head[0], head[2], direction)) {
     return KindaSafe;
   } else {
     return NotSafe;
@@ -81,24 +57,47 @@ int main() {
   FILE *file = fopen(FILE_NAME, "r");
   if (!file)
     return 0;
-  char line[MAX_STRING_LEN];
-  char *head;
-  int i, direction, safeness, number_safe = 0;
-  Window window;
-  while (fgets(line, MAX_STRING_LEN, file)) {
-    head = line;
-    safeness = 2;
-    for (i = 0; i < 4; i++) {
-      window.pane[i] = NEXT_INT_ON_LINE();
+  char strline[MAX_STRING_LEN], *strhead;
+  int intline[MAX_LEVELS + 1], *inthead;
+  int i, level, direction, report_safeness, value_safeness, advancement,
+      number_safe = 0;
+  while (fgets(strline, MAX_STRING_LEN, file)) {
+    strhead = strline;
+    inthead = intline;
+    // SAFETY: assume that MAX_LEVELS is correct
+    for (i = 0; (level = strtol(strhead, &strhead, 10)) != 0; i++) {
+      printf("%d ", level);
+      intline[i] = level;
     }
-    direction = init_direction(window);
-    safeness -= init_window_safeness(window, direction);
-    while ((safeness -= window_safeness(window, direction)) > 0 &&
-           slide_window(&window, NEXT_INT_ON_LINE()))
-      ;
-    safeness -= final_window_safeness(window, direction);
-    printf("%d\n", safeness);
-    number_safe += (safeness > 0);
+    printf(":: ");
+    direction = init_direction(inthead);
+    value_safeness = window_edge_safeness(inthead, direction);
+    report_safeness = 2 - value_safeness;
+    advancement = value_safeness + 1;
+    inthead += advancement;
+    i -= advancement;
+    while (i > 2) {
+      /* printf("%d ", inthead[0]); */
+      value_safeness = window_safeness(inthead, direction);
+      report_safeness -= value_safeness;
+      advancement = value_safeness + 1;
+      inthead += advancement;
+      i -= advancement;
+    }
+    /* if (i == 1) { */
+    /*   printf("!"); */
+    /*   // we've got one more to check, */
+    /*   // but we do need to move the head back before that */
+    /*   i++; */
+    /*   inthead--; */
+    /* } */
+    if (i == 2) {
+      printf("! ");
+      // we've still got one more to check
+      report_safeness -= window_edge_safeness(inthead, direction);
+    }
+    printf(":: %d\n", report_safeness);
+    number_safe += (report_safeness > 0);
   }
   printf("%d\n", number_safe);
   return 0;
