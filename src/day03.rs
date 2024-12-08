4434a895a114df58b89d155f092c9968082cc559
JT992 <jake@artran.co.uk>
2024-12-08 12:17:46 +0000

    03 Change + to {1,3}

--- src/day03.rs
+++ src/day03.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 
 pub fn main(file: &str) -> usize {
-    let re = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+    let re = Regex::new(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)").unwrap();
     re.captures_iter(file)
         .map(|c| {
             let (_, [first, second]) = c.extract();
